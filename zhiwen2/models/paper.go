0b575243eceb32e76e1e426ecc6a2107813f84bf
choumin <zhoumin@bupt.edu.cn>
2019-07-10 08:46:48 +0800

    add paper controller

---
+++ zhiwen2/models/paper.go
@@ -0,0 +1,9 @@
+package models
+
+func GetPaperTitleById(id string) string {
+	return "paper title " + id
+}
+
+func GetPaperContentById(id string) string {
+	return "paper content " + id
+}
---
+++ zhiwen2/views/paper.tpl
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>{{.Title}}</title>
+    </head>
+    <body>
+        {{.Content}}<br />
+        {{.Number}}<br />
+    </body>
+</html>
